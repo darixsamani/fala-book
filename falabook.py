637e42346de3efc9aa6c9e25e4b11f042646f48a
darix samani <samanidarix@gmail.com>
2020-07-11 15:44:29 +0100

    creation d'un script Python pour recherches les livres sur le site pdfdrive.net

--- falabook.py
+++ falabook.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import requests
 import requests
 import time
@@ -7,11 +8,39 @@ from bs4 import BeautifulSoup
 import lxml
 import csv
 
-url = "https://www.pdfdrive.com" 
+# all categories 78
 
+url = "https://www.pdfdrive.com/search"
 
-reponse = requests.get(url) 
+payload ={'q':'java' , 'pagecount':'', 'pubyear':'', 'searchin':'','em':'','page':'1'}
+reponse = requests.get(url, params=payload ) 
 
 if reponse.ok:
-    soup = BeautifulSoup( reponse.text, "lxml")
-    print(soup)
+    html=reponse.text
+    soup = BeautifulSoup( html,  "lxml")
+
+    livres = soup.find_all( 'div', {'class':'file-right'}) # recupere tous les libres
+    for livre in livres:
+        link_livre = livre.find('a').get('href')
+        try:
+            page_count = livre.find( 'span', {'class', 'fi-pagecount'}).get_text()
+        except:
+            page_count = "inconnu"
+        
+        year = livre.find( 'span', { 'class', 'fi-year'}).get_text()
+        size = livre.find( 'span', { 'class', 'fi-size hidemobile'}).get_text()
+        number_down = livre.find( 'span', { 'class', 'fi-hit'}).get_text()
+        try:
+            new = livre.find( 'span', {'class', 'new'} ).get_text()
+        except:
+            new = "old"
+        
+
+        print(" |||+ link livre :",link_livre)
+        print(" |||+ page count : ",page_count)
+        print(" |||+ year : ",year)
+        print(" |||+ size : ",size)
+        print(" |||+  number_down : ",number_down)
+        print(" |||+ new : ",new)
+        print(" --------------------------------------")
+
